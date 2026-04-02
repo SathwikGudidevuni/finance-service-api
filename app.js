1db5dd1011a8b75841f0793915cc11e183bdfe04
SathwikGudidevuni <sathwik.gudidevuni2006@gmail.com>
2026-04-02 21:14:29 +0530

    Connect user routes to Express app

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const express = require("express")
 const app = express();
 const PORT = 3000;
 
+const usersRoutes = require("./routes/userRoutes");
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Server is running successfully!");
 });
+
+app.use("/", usersRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
