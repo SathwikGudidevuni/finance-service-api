b809b54ee4f15084322dd00b98d145f3679c289e
SathwikGudidevuni <sathwik.gudidevuni2006@gmail.com>
2026-04-05 21:15:37 +0530

    Added rate limiting to API routes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const rateLimit = require("express-rate-limit");
 require("dotenv").config();
 
 const app = express();
@@ -8,15 +9,23 @@ const usersRoutes = require("./routes/userRoutes");
 const dashboardRoutes = require("./routes/dashboardRoutes");
 const recordRoutes = require("./routes/recordRoutes");
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: {
+    message: "Too many requests, please try again later"
+  }
+});
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Server is running successfully!");
 });
 
-app.use("/", dashboardRoutes);
-app.use("/", usersRoutes);
-app.use("/", recordRoutes);
+app.use("/", limiter, dashboardRoutes);
+app.use("/", limiter, usersRoutes);
+app.use("/", limiter, recordRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
